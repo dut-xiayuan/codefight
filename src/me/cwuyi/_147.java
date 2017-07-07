021905f77ed2d5efd32f7e998f11d13e49bf2ebb
xiayuan <xiayuan1992@gmail.com>
2017-07-07 16:06:04 +0800

    binary tree traverse

--- src/me/cwuyi/_147.java
+++ src/me/cwuyi/_147.java
@@ -1,21 +1,32 @@
 package me.cwuyi;
 
+import java.util.*;
+
 /**
  * Created by XIA on 2017/7/5.
  */
 public class _147 {
     public ListNode insertionSortList(ListNode head) {
-        ListNode p = head;
-        ListNode l = head.next;
-
-        while (l != null) {
-            ListNode temp = p;
-            while (p.val < l.val) {
-                p = p.next;
+        Set<ListNode> set = new TreeSet<>(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((ListNode)o1).val - ((ListNode)o2).val;
             }
+        });
 
+        ListNode now = head;
+        while (now != null ) {
+            set.add(now);
+            now = now.next;
         }
-        return p.next;
+
+        ListNode holder = new ListNode(0);
+        now = holder;
+        for (ListNode node : set) {
+            now.next = node;
+            now = now.next;
+        }
+        return holder.next;
     }
 
 }

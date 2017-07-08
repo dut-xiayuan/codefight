56ab5d3b8f752b2c4c673dc98423a08b253ea160
xiayuan <xiayuan1992@gmail.com>
2017-07-08 19:22:53 +0800

    list node loop detect

---
+++ src/me/cwuyi/_140.java
@@ -0,0 +1,15 @@
+package me.cwuyi;
+
+import java.util.List;
+
+/**
+ * Created by XIA on 2017/7/8.
+ */
+public class _140 {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+
+
+
+        return null;
+    }
+}
